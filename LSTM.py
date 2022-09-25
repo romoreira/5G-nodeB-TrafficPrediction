4ea8cb2750faf077a09c9445722835eff23ba3e6
Rodrigo <rodrigo.moreira206@gmail.com>
2022-09-25 16:49:07 -0300

    Apparently it is working - missing to understand the Forecasting method

--- LSTM.py
+++ LSTM.py
@@ -13,6 +13,9 @@ import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from math import sqrt
 from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_error
+from tsai.models.utils import *
+criterion = nn.CrossEntropyLoss()
+
 
 def check_error(orig, pred, name_col='', index_name=''):
     bias = np.mean(orig - pred)
@@ -51,200 +54,30 @@ def create_loss_graph(y1, y2, model_name):
 
     # plt.grid()
     # plt.figure()
-    plt.savefig('Resultados/' + 'loss-graf_' + model_name + '.pdf')
+    plt.savefig('Resultados/' +str(model_name)+ '_loss-graf.png')
     # plt.show()
 
-
-def create_graph(df, name):
+def plot_real_pred_alone(df_out, model_name):
+    #Creating graph
+    print("Df_out")
+    df_out = df_out.rename(columns={"Model forecast": "Predicted"})
     plot_template = dict(
-        layout=go.Layout({
-            "font_size": 18,
-            "xaxis_title_font_size": 24,
-            "yaxis_title_font_size": 24})
-    )
-
-    fig = px.line(df, labels=dict(
-        created_at="Date", value="Resources Usage", variable="Sensor"
-    ))
+            layout=go.Layout({
+                "font_size": 18,
+                "xaxis_title_font_size": 24,
+                "yaxis_title_font_size": 24})
+        )
+    print("DF_OUT dentro do plot: "+str(df_out))
+    fig = px.line(df_out, labels=dict(created_at="Date", value="Resources Usage"))
+    fig.add_vline(x=test_start, line_width=4, line_dash="dash")
+    fig.add_annotation(xref="paper", x=0.75, yref="paper", y=0.8, text="Test set start", showarrow=False)
     fig.update_layout(
         template=plot_template, legend=dict(orientation='h', y=1.02, title_text="")
     )
-
-    fig.write_image("Resultados/"+str(name)+"_file.pdf")
-
-pio.templates.default = "plotly_white"
-
-file_name = "dataset.pkl"
-
-df = pd.read_pickle(file_name)
-df = df['LesCorts']
-df.set_index(df.iloc[:, 0].name)
-df.index.names = ['TimeStamp']
-
-
-
-data_columns = list(df.columns.values)
-data = df[data_columns].values
-data = np.clip(data, 0.0, np.percentile(data.flatten(), 99))  # we use 99% as the threshold
-df[data_columns] = data
-
-aggregated_time_series = np.sum(data, axis=1)
-df_ts = pd.DataFrame()
-df_ts['data'] = aggregated_time_series / 1000  # Plot in Mbps
-
-#df.drop(df.columns[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]], axis=1, inplace=True)
-df = df.assign(aggregated_ts=df_ts['data'].tolist())
-
-df.fillna(0, inplace=True)
-
-print(df)
-create_graph(df, "DF_Original")
-
-
-
-#Normalizing the aggregated column
-df_min_max_scaled = df.copy()
-column = 'aggregated_ts'
-df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (
-            df_min_max_scaled[column].max() - df_min_max_scaled[column].min())
-print(df_min_max_scaled)
-df = df_min_max_scaled
-#print(df)
-#create_graph(df_min_max_scaled, "DF_Normalized")
-#exit()
-
-
-
-target_sensor = "aggregated_ts"
-features = list(df.columns.difference([target_sensor]))
-forecast_lead = 2
-target = f"{target_sensor}_lead{forecast_lead}"
-df[target] = df[target_sensor].shift(-forecast_lead)
-df = df.iloc[:-forecast_lead]
-print(df)
-
-
-test_start = "2019-01-20"
-df_train = df.loc[:test_start].copy()
-df_test = df.loc[test_start:].copy()
-print("Test set fraction:", len(df_test) / len(df))
-
-
-target_mean = df_train[target].mean()
-target_stdev = df_train[target].std()
-for c in df_train.columns:
-    mean = df_train[c].mean()
-    stdev = df_train[c].std()
-    df_train[c] = (df_train[c] - mean) / stdev
-    df_test[c] = (df_test[c] - mean) / stdev
-
-class SequenceDataset(Dataset):
-    def __init__(self, dataframe, target, features, sequence_length=5):
-        self.features = features
-        self.target = target
-        self.sequence_length = sequence_length
-        self.y = torch.tensor(dataframe[target].values).float()
-        self.X = torch.tensor(dataframe[features].values).float()
-
-    def __len__(self):
-        return self.X.shape[0]
-
-    def __getitem__(self, i):
-        if i >= self.sequence_length - 1:
-            i_start = i - self.sequence_length + 1
-            x = self.X[i_start:(i + 1), :]
-        else:
-            padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
-            x = self.X[0:(i + 1), :]
-            x = torch.cat((padding, x), 0)
-
-        return x, self.y[i]
-
-
-i = 27
-sequence_length = 4
-train_dataset = SequenceDataset(
-    df_train,
-    target=target,
-    features=features,
-    sequence_length=sequence_length
-)
-
-print("Target: "+str(target))
-print("Features: "+str(features))
-print("sequence_length: "+str(sequence_length))
-
-X, y = train_dataset[i]
-print(X)
-
-
-#reating the dataset and the data loaders for real
-torch.manual_seed(101)
-
-batch_size = 32
-sequence_length = 30
-
-train_dataset = SequenceDataset(
-    df_train,
-    target=target,
-    features=features,
-    sequence_length=sequence_length
-)
-test_dataset = SequenceDataset(
-    df_test,
-    target=target,
-    features=features,
-    sequence_length=sequence_length
-)
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-X, y = next(iter(train_loader))
-
-print("Features shape:", X.shape)
-print("Target shape:", y.shape)
-
-
-
-#Creating the model
-class ShallowRegressionLSTM(nn.Module):
-    def __init__(self, num_sensors, hidden_units):
-        super().__init__()
-        self.num_sensors = num_sensors  # this is the number of features
-        self.hidden_units = hidden_units
-        self.num_layers = 1
-
-        self.lstm = nn.LSTM(
-            input_size=num_sensors,
-            hidden_size=hidden_units,
-            batch_first=True,
-            num_layers=self.num_layers
-        )
-
-        self.linear = nn.Linear(in_features=self.hidden_units, out_features=1)
-
-    def forward(self, x):
-        batch_size = x.shape[0]
-        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
-        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
-
-        _, (hn, _) = self.lstm(x, (h0, c0))
-        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
-
-        return out
-
-
-#Quality function
-learning_rate = 5e-5
-num_hidden_units = 16
-
-model = ShallowRegressionLSTM(num_sensors=len(features), hidden_units=num_hidden_units)
-loss_function = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    fig.write_image("Resultados/"+str(model_name)+"_Real_versus_Pred_graph.png")
 
 #Plotting function
-def plot_real_versus_predict(data, model_name, figsize=(12, 9), lags=24, rotation=0):
+def plot_real_pred_detailed(data, model_name, figsize=(12, 9), lags=24, rotation=0):
     # Creating the column error
     data['Error'] = data.iloc[:, 0] - data.iloc[:, 1]
 
@@ -273,7 +106,7 @@ def plot_real_versus_predict(data, model_name, figsize=(12, 9), lags=24, rotatio
     plot_acf(data.iloc[:, 2], lags=lags, zero=False, ax=ax4)
     plt.tight_layout()
     plt.show()
-    plt.savefig("Resultados/" + str(model_name) + '_autoCorrelation.pdf', bbox_inches='tight', pad_inches=0.1)
+    plt.savefig("Resultados/" + str(model_name) + '_autoCorrelation.png', bbox_inches='tight', pad_inches=0.1)
 
 #Training phase
 def train_model(data_loader, model, loss_function, optimizer):
@@ -310,6 +143,217 @@ def test_model(data_loader, model, loss_function):
     print(f"Test loss: {avg_loss}")
     return avg_loss
 
+def create_graph(df, name):
+    plot_template = dict(
+        layout=go.Layout({
+            "font_size": 18,
+            "xaxis_title_font_size": 24,
+            "yaxis_title_font_size": 24})
+    )
+
+    fig = px.line(df, labels=dict(
+        created_at="Date", value="Resources Usage", variable="Sensor"
+    ))
+    fig.update_layout(
+        template=plot_template, legend=dict(orientation='h', y=1.02, title_text="")
+    )
+
+    fig.write_image("Resultados/"+str(name)+"_file.png")
+
+
+
+class SequenceDataset(Dataset):
+    def __init__(self, dataframe, target, features, sequence_length=5):
+        self.features = features
+        self.target = target
+        self.sequence_length = sequence_length
+        self.y = torch.tensor(dataframe[target].values).float()
+        self.X = torch.tensor(dataframe[features].values).float()
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, i):
+        if i >= self.sequence_length - 1:
+            i_start = i - self.sequence_length + 1
+            x = self.X[i_start:(i + 1), :]
+        else:
+            padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
+            x = self.X[0:(i + 1), :]
+            x = torch.cat((padding, x), 0)
+
+        return x, self.y[i]
+
+
+#Creating the model
+class ShallowRegressionLSTM(nn.Module):
+    def __init__(self, num_sensors, hidden_units):
+        super().__init__()
+        self.num_sensors = num_sensors  # this is the number of features
+        self.hidden_units = hidden_units
+        self.num_layers = 1
+
+        self.lstm = nn.LSTM(
+            input_size=num_sensors,
+            hidden_size=hidden_units,
+            batch_first=True,
+            num_layers=self.num_layers
+        )
+
+        self.linear = nn.Linear(in_features=self.hidden_units, out_features=1)
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+
+        _, (hn, _) = self.lstm(x, (h0, c0))
+        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
+
+        return out
+
+def create_model(model_name, num_features, num_hidden_units, sequence_length, batch_size):
+    model = ""
+    if model_name == "LSTM":
+        model =  ShallowRegressionLSTM(num_sensors=num_features, hidden_units=num_hidden_units)
+        return model
+    elif model_name == "ResNet":
+        model = ResNet(sequence_length, batch_size)
+        return model
+    elif model_name == "InceptionTime":
+        model = InceptionTime(sequence_length, batch_size)
+        return model
+    elif model_name == "FCN":
+        model = FCN(sequence_length,batch_size)
+        return model
+    elif model_name == "OmniScaleCNN":
+        print("Hello")
+        model = OmniScaleCNN(num_features, batch_size, sequence_length)
+        return model
+    else:
+        print("Unable to find any model with this name")
+        exit()
+
+pio.templates.default = "plotly_white"
+
+file_name = "dataset.pkl"
+
+df = pd.read_pickle(file_name)
+df = df['LesCorts']
+df.set_index(df.iloc[:, 0].name)
+df.index.names = ['TimeStamp']
+
+
+
+data_columns = list(df.columns.values)
+data = df[data_columns].values
+data = np.clip(data, 0.0, np.percentile(data.flatten(), 99))  # we use 99% as the threshold
+df[data_columns] = data
+
+aggregated_time_series = np.sum(data, axis=1)
+df_ts = pd.DataFrame()
+df_ts['data'] = aggregated_time_series / 1000  # Plot in Mbps
+
+#df.drop(df.columns[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]], axis=1, inplace=True)
+df = df.assign(aggregated_ts=df_ts['data'].tolist())
+
+df.fillna(0, inplace=True)
+
+print(df)
+create_graph(df, "LSTM_DF_Original")
+
+
+
+#Normalizing the aggregated column
+df_min_max_scaled = df.copy()
+column = 'aggregated_ts'
+df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (
+            df_min_max_scaled[column].max() - df_min_max_scaled[column].min())
+print(df_min_max_scaled)
+df = df_min_max_scaled
+#print(df)
+#create_graph(df_min_max_scaled, "DF_Normalized")
+#exit()
+
+
+
+target_sensor = "aggregated_ts"
+features = list(df.columns.difference([target_sensor]))
+forecast_lead = 30
+target = f"{target_sensor}_lead{forecast_lead}"
+df[target] = df[target_sensor].shift(-forecast_lead)
+df = df.iloc[:-forecast_lead]
+print(df)
+
+
+test_start = "2019-01-20"
+df_train = df.loc[:test_start].copy()
+df_test = df.loc[test_start:].copy()
+print("Test set fraction:", len(df_test) / len(df))
+
+
+target_mean = df_train[target].mean()
+target_stdev = df_train[target].std()
+for c in df_train.columns:
+    mean = df_train[c].mean()
+    stdev = df_train[c].std()
+    df_train[c] = (df_train[c] - mean) / stdev
+    df_test[c] = (df_test[c] - mean) / stdev
+
+
+#reating the dataset and the data loaders for real
+torch.manual_seed(101)
+
+batch_size = 32
+sequence_length = 30
+
+train_dataset = SequenceDataset(
+    df_train,
+    target=target,
+    features=features,
+    sequence_length=sequence_length
+)
+test_dataset = SequenceDataset(
+    df_test,
+    target=target,
+    features=features,
+    sequence_length=sequence_length
+)
+
+print("Target: "+str(target))
+print("Features: "+str(features))
+print("sequence_length: "+str(sequence_length))
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+X, y = next(iter(train_loader))
+
+print("Features shape:", X.shape)
+print("Target shape:", y.shape)
+
+#Quality function
+learning_rate = 5e-5
+num_hidden_units = 16
+
+#xb = torch.rand(2, 3, 4)
+#print(xb)
+#print(X.shape)
+#print(y.shape)
+#model = ResNet(sequence_length, batch_size)
+#print(model)
+#exit()
+
+#model = create_model("OmniScaleCNN", num_features=len(features), num_hidden_units=num_hidden_units, sequence_length=sequence_length, batch_size=batch_size)
+#model = create_model("FCN", num_features=len(features), num_hidden_units=num_hidden_units, sequence_length=sequence_length, batch_size=batch_size)
+#model = create_model("InceptionTime", num_features=len(features), num_hidden_units=num_hidden_units, sequence_length=sequence_length, batch_size=batch_size)
+#model = create_model("ResNet", num_features=len(features), num_hidden_units=num_hidden_units, sequence_length=sequence_length, batch_size=batch_size)
+model = create_model("LSTM", num_features=len(features), num_hidden_units=num_hidden_units, sequence_length=sequence_length, batch_size=batch_size)
+#model = ShallowRegressionLSTM(num_sensors=len(features), hidden_units=num_hidden_units, sequence_length, batch_size)
+loss_function = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+print("type of model: "+str(model))
+
 
 print("Untrained test\n--------")
 test_model(test_loader, model, loss_function)
@@ -332,43 +376,29 @@ def predict(data_loader, model):
         for X, _ in data_loader:
             y_star = model(X)
             output = torch.cat((output, y_star), 0)
-
     return output
 
 
 train_eval_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
 
+
+
 ystar_col = "Model forecast"
 df_train[ystar_col] = predict(train_eval_loader, model).numpy()
+
 df_test[ystar_col] = predict(test_loader, model).numpy()
 
 df_out = pd.concat((df_train, df_test))[[target, ystar_col]]
+
 
 for c in df_out.columns:
     df_out[c] = df_out[c] * target_stdev + target_mean
 
 print(df_out)
-plot_real_versus_predict(df_out, "LSTM")
 
-
-
-
-#Creating graph
-plot_template = dict(
-        layout=go.Layout({
-            "font_size": 18,
-            "xaxis_title_font_size": 24,
-            "yaxis_title_font_size": 24})
-    )
-fig = px.line(df_out, labels=dict(created_at="Date", value="Resources Usage"))
-fig.add_vline(x=test_start, line_width=4, line_dash="dash")
-fig.add_annotation(xref="paper", x=0.75, yref="paper", y=0.8, text="Test set start", showarrow=False)
-fig.update_layout(
-    template=plot_template, legend=dict(orientation='h', y=1.02, title_text="")
-)
-fig.write_image("Resultados/Real_versus_Predito_graph.pdf")
-
-
-check_error(df_out[['aggregated_ts_lead2']].to_numpy(), df_out[['Model forecast']].to_numpy(), name_col="LSTM")
+df_out.rename(columns={ df_out.columns[0]: "Real" }, inplace = True)#Rename Pandas Dataframe
+plot_real_pred_detailed(df_out, "LSTM")
+plot_real_pred_alone(df_out.iloc[:, :-1], "LSTM")
+check_error(df_out[['Real']].to_numpy(), df_out[['Model forecast']].to_numpy(), name_col="LSTM")
 create_loss_graph(train_loss, test_loss, "LSTM")
 
